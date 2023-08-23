3becbff734243a3346610024b5bb46b12dedf7c6
naomieliana7 <131322122+naomieliana7@users.noreply.github.com>
2023-08-23 10:53:40 -0400

    Add files via upload

--- Core/cuenta_bancaria.py
+++ Core/cuenta_bancaria.py
@@ -14,8 +14,7 @@ class CuentaBancaria:
         return self
     
     def mostrar_info_cuenta(self):
-        print("Balance: ", self.balance)
-        return self
+            return self.balance
     
     def generar_interés(self):
         self.balance += (self.balance*self.tasa_interés)
@@ -26,10 +25,40 @@ class CuentaBancaria:
         for cuenta in cls.cuentas: 
             print(f"Balance: {cuenta.balance}, Tasa de Interés: {cuenta.tasa_interés}")
 
-cuenta1 = CuentaBancaria(0.01, 0)
-cuenta2 = CuentaBancaria(0.015, 0)
+class Usuario:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.cuenta = CuentaBancaria(0.02, 0)
+    # agregando el método de depósito
+    def hacer_depósito(self, amount):	# toma un argumento que es el monto del depósito
+        self.cuenta.depósito(amount)	# la cuenta del usuario específico aumenta en la cantidad del valor recibido
+        return self
 
-cuenta1.depósito(50).depósito(100).depósito(150).retiro(25).generar_interés().mostrar_info_cuenta()
-cuenta2.depósito(150).depósito(200).retiro(35).retiro(15).retiro(20).retiro(50).generar_interés().mostrar_info_cuenta()
+    # agregando método de retiro de dinero
+    def hacer_retiro(self, amount):
+        self.cuenta.retiro(amount)
+        return self
 
-CuentaBancaria.imprimir_cuentas()
+    #agregando método de reetiro
+    def mostrar_balance_usuario(self): 
+        print("Usuario:", self.name, ", Balance:", self.cuenta.mostrar_info_cuenta())
+        return self
+
+    #agregando método de transferencia de dinero 
+    def transfer_dinero(self, other_user, amount):
+        self.cuenta.retiro(amount)
+        other_user.cuenta.depósito(amount)
+        print(self.name, "ha tranferido", amount, "a", other_user.name)
+        
+
+
+guido = Usuario("Guido van Rossum", "guido@python.com")
+monty = Usuario("Monty Python", "monty@python.com")
+rafael = Usuario("Rafael Escobar", "rafael@python.com")
+
+guido.hacer_depósito(100).hacer_depósito(200).hacer_depósito(300).hacer_retiro(50).mostrar_balance_usuario()
+monty.hacer_depósito(60).hacer_depósito(121).hacer_retiro(16).hacer_retiro(24).mostrar_balance_usuario()
+rafael.hacer_depósito(200).hacer_retiro(35).hacer_retiro(62).hacer_retiro(25).mostrar_balance_usuario()
+
+monty.transfer_dinero(rafael, 25)
